f22bee0c0f1e9769febfd66ed20702e1c6922083
hsbx520 <79846141+hsbx520@users.noreply.github.com>
2025-07-10 03:13:00 +0800

    Update script.js

--- script.js
+++ script.js
@@ -580,7 +580,7 @@ class PurchaseNotificationSystem {
     calculateTokensFromAmount(amount, currency) {
         // Use the exact rates from the presale
         const rates = {
-            'ETH': 225000,
+            'ETH': 239360,
             'BNB': 58000,
             'USDT': 88,
             'USDC': 88
