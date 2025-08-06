07af4d3418a930e72e8cac16f7dbcb32b7ba6a0b
hsbx520 <79846141+hsbx520@users.noreply.github.com>
2025-08-06 11:13:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -309,8 +309,8 @@ document.querySelectorAll('.faq-question').forEach(question => {
 
 // Countdown Timer Logic
 function startCountdown() {
-    // Target date: July 18, 2025, 12:00 PM US Eastern Time (EDT which is UTC-4)
-    const targetDate = new Date('2025-07-18T12:00:00-04:00').getTime();
+    // Target date: August 10, 2025, 12:00 PM US Eastern Time (EDT which is UTC-4)
+    const targetDate = new Date('2025-08-10T12:00:00-04:00').getTime();
     
     const countdownTimerEl = document.getElementById('countdown-timer');
     const daysEl = document.getElementById('days');
